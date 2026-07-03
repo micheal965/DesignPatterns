7b2332549ed0a8174d8421d2fb3bc514edf78df3
Micheal Ghobrial <michealghobriall@gmail.com>
2026-07-03 23:21:32 +0300

    Create initial .NET project scaffold for FacadePattern

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FacadePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
